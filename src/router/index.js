3b2f83021cc829895e462bb3f32782b2c83ceb71
Tina Shih <s6865835@gmail.com>
2019-12-18 20:24:28 +0800

    Remove the default page from vue and create a basic home page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import {
+  Home,
+} from '../views';
 
 Vue.use(VueRouter);
 
@@ -10,18 +12,9 @@ const routes = [
     name: 'home',
     component: Home,
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
 ];
 
 const router = new VueRouter({
-  mode: 'history',
   base: process.env.BASE_URL,
   routes,
 });
---
+++ src/views/Home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    Home Page
+  </div>
+</template>
+
+<script>
+import Home from './Home';
+
+export default Home;
+</script>
+
+<style lang="scss" scoped>
+  @import 'Home';
+</style>
